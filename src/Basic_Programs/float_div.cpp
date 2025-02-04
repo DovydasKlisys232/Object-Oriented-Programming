3e6369c5516322f5e62b15c2a8a0b22a8bb1428c
DovydasKlisys232 <B00165094@mytudublin.ie>
2025-02-04 20:35:15 +0000

    updates

---
+++ src/Basic_Programs/float_div.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+//first variable are declared with float values
+//then they are printed out with the result of the computation
+int main()
+{
+    float a = 2.0;
+    float b = 5.0;
+    cout<<a<<" divided by "<<b<<" is "<<a/b;
+    return 0;
+}
---
+++ src/Basic_Programs/hello.cpp
@@ -0,0 +1,18 @@
+/*
+Programmer: Dovydas Klisys
+Date: 4th Feb 2025
+ID: B00165094
+Program: Simple program that prints out hello world onto the terminal
+*/
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World!\n";
+    return 0;
+}
